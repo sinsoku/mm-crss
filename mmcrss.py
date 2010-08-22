34be1d82bfee2d770967c79556ad581f6aeff0ba
sinsoku <sinsoku.listy@gmail.com>
2010-08-22 20:02:03 +0900

    Feed Parserをimportするように修正

--- mmcrss.py
+++ mmcrss.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import sys
+sys.path.append('feedparser-4.1.zip')
+import feedparser
 from google.appengine.ext import webapp
+
+class MMCommentRSS:
+    def __init__(self):
+        pass
 
 class MMCommentRSSHandler(webapp.RequestHandler):
     def get(self):
