7c8b67c5e5edfdedf2014bd005afc6e51215f6d2
sinsoku <sinsoku.listy@gmail.com>
2010-08-28 17:44:56 +0900

    インスタンス生成だけのテストを追加

--- test_mmcrss.py
+++ test_mmcrss.py
@@ -1,3 +1,8 @@
 from webtest import TestApp
-from mmcrss import MMCommentRSS, MMCommentRSSHandler
+from mmcrss import *
 
+def test_comment_parser():
+    ecp = ExtractCommentParser()
+
+def test_mmcrss():
+    mmcrss = MMCommentRSS()
